c2b81c212b0a6772e516b8e6080d5985e5cda6b2
David Ledesma <desarrollo.centro8@vemasmas.mx>
2025-04-14 16:00:06 -0600

    h

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,21 @@
 from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+import os
 
-# 🔽 IMPORTA TUS MODELOS AQUÍ
-from models import Peticion, Usuario
+# Cargar variables de entorno desde archivo .env (local)
+load_dotenv()
 
-DATABASE_URL = ("postgresql://postgres.xpzlzwkusueordfomcxj:Hola0810-10@aws-0-us-east-1.pooler.supabase.com:6543/postgres")
+# Leer URL desde variable de entorno (Render o local)
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./default.db")
+
+# Crear el engine
 engine = create_engine(DATABASE_URL, echo=True)
 
+# Inicializar base de datos
 def init_db():
     SQLModel.metadata.create_all(engine)
 
+# Obtener sesión
 def get_session():
     with Session(engine) as session:
         yield session
